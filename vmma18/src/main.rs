d93fc2374500940b62a2074ca03bb697f9533f8a
Ryan Perry <rperry21@vols.utk.edu>
2025-04-30 10:17:43 -0400

    added main.rs; Cargo.toml

---
+++ vmma18/src/main.rs
@@ -0,0 +1,9 @@
+/* 
+ * Names: Tyler Cartier, Thomas Latawiec, Ryan Perry, Chase Woodfill
+ * Date: 05/06/2025
+ * Synopsis: Runs 4-byte instructions in an assembled binary file
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
